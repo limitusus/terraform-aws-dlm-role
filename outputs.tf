37dde07c762db1bcb3680d6a2954047854561c52
Tomoya Kabe <limit.usus@gmail.com>
2021-06-11 01:15:16 +0900

    Output role name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "role_arn" {
   description = "Role ARN"
   value       = aws_iam_role.this.arn
 }
+
+output "role_name" {
+  description = "Role name"
+  value       = aws_iam_role.this.name
+}
